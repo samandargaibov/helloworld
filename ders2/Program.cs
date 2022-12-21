a17cb9be71cfe40c02b20ce07839fbad62892a98
Gaibov Samandar <SamandarrG@code.local>
2022-12-21 19:46:33 +0400

    add sum

--- ders2/Program.cs
+++ ders2/Program.cs
@@ -6,7 +6,14 @@ namespace ders2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("1ci ededi daxil edin:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2ci ededi daxil edin: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+
+            Console.WriteLine($"Cemi: {a}+{b}={c}");
         }
     }
 }
